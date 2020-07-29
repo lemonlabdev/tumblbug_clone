f321978f7638398d0aba37a8c3ab136548cd7c38
plantstoen <plantstoen@gmail.com>
2020-07-29 11:37:36 +0900

    test: pull&request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import Link from "next/link";
-import { GetStaticProps } from "next";
+import Head from 'next/head'
+import Link from 'next/link'
+import { GetStaticProps } from 'next'
 
 interface HomeProsInterface {}
 
@@ -10,7 +10,8 @@ export default function Home() {
       <Head>
         <title>텀블벅 클론코딩</title>
       </Head>
+      <span>테스트 풀리퀘</span>
       <div>{/*여기다 작성하기*/}</div>
     </div>
-  );
+  )
 }
