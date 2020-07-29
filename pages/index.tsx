9b8fb95b9ddfcac8ac5bad9ef1c015b752a04d71
plantstoen <plantstoen@gmail.com>
2020-07-29 11:19:03 +0900

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
+import { GetStaticProps } from "next";
+
+interface HomeProsInterface {}
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>텀블벅 클론코딩</title>
+      </Head>
+      <div>{/*여기다 작성하기*/}</div>
+    </div>
+  );
+}
