5b7e142c96e56af5b129b4053d6083193fe6fa4f
Taeyeon <ruadl034@naver.com>
2020-07-29 15:47:27 +0900

    feat: HeaderTextButton.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { GetStaticProps } from 'next'
+import HeaderTextButton from '../components/atoms/HeaderTextButton'
 
 interface HomeProsInterface {}
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Head>
         <title>텀블벅 클론코딩</title>
       </Head>
+      <HeaderTextButton>프로젝트 둘러보기</HeaderTextButton>
       <span>테스트 풀리퀘</span>
       <div>{/*여기다 작성하기*/}</div>
     </div>
