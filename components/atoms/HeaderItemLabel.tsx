e2a7e12ac13e0a5ec1af054d7aee9a253ee3a05b
Taeyeon <ruadl034@naver.com>
2020-07-30 17:04:38 +0900

    feat: add image

--- components/atoms/HeaderItemLabel.tsx
+++ components/atoms/HeaderItemLabel.tsx
@@ -6,9 +6,11 @@ type HeaderLabelProps = {
 };
 
 const Text = styled.span`
+  @media (min-width: 1080px) {
+    line-height: 65px;
+  }
   color: inherit;
   font-family: 'Noto Sans KR', sans-serif;
-
   &:hover {
     cursor: pointer;
     color: rgba(0, 0, 0, 0.7);
